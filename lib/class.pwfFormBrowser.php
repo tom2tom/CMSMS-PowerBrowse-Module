ccbeca1a9aef6bca088124bd23e534d76e18ce65
tom2tom <tpgww@onepost.net>
2015-06-12 22:39:08 +1000

    omit comment

--- lib/class.pwfFormBrowser.php
+++ lib/class.pwfFormBrowser.php
@@ -1,7 +1,6 @@
 <?php
 # This file is part of CMS Made Simple module: PowerBrowse
 # Copyright (C) 2011-2015 Tom Phane <tpgww@onepost.net>
-# Derived in part from FormBuilder-module file (C) 2005-2012 Samuel Goldstein <sjg@cmsmodules.com>
 # Refer to licence and other details at the top of file PowerBrowse.module.php
 # More info at http://dev.cmsmadesimple.org/projects/powerbrowse
 
