6fb377a0e32a7f47ed8f887781daae15235258b5
tom2tom <tpgww@onepost.net>
2017-07-21 23:32:30 +1000

    indicative code for password reconstruct

--- method.upgrade.php
+++ method.upgrade.php
@@ -1,13 +1,27 @@
 <?php
 # This file is part of CMS Made Simple module: PWFBrowse
-# Copyright (C) 2011-2016 Tom Phane <tpgww@onepost.net>
-# Derived in part from FormBrowser-module file (C) 2006-2011 Samuel Goldstein <sjg@cmsmodules.com>
+# Copyright (C) 2011-2017 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file PWFBrowse.module.php
-# More info at http://dev.cmsmadesimple.org/projects/PWFBrowse
+# More info at http://dev.cmsmadesimple.org/projects/powerbrowse
 
 if (!$this->_CheckAccess('modify')) {
 	exit;
 }
 
 switch ($oldversion) {
+/* case '0.8':
+	$cfuncs = new PWFBrowse\Crypter($this);
+	$key = 'masterpass';
+	$s = base64_decode($this->GetPreference($key));
+	$t = $config['ssl_url'].$this->GetModulePath();
+	$val = hash('crc32b',$this->GetPreference('nQCeESKBr99A').$t);
+	$pw = $cfuncs->decrypt($s,$val);
+	if (!$pw) {
+		$pw = base64_decode('');
+	}
+	$this->RemovePreference($key);
+	$this->RemovePreference('nQCeESKBr99A');
+	$cfuncs->init_crypt();
+	$cfuncs->encrypt_preference(PWFBrowse\Crypter::MKEY,$pw);
+*/
 }
