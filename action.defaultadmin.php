c377897155199deee547c2bb2892ba7ce15b29ca
tom2tom <tpgww@onepost.net>
2017-07-21 23:31:56 +1000

    cleanup licence text

--- action.defaultadmin.php
+++ action.defaultadmin.php
@@ -1,8 +1,8 @@
 <?php
 # This file is part of CMS Made Simple module: PWFBrowse
-# Copyright (C) 2011-2016 Tom Phane <tpgww@onepost.net>
+# Copyright (C) 2011-2017 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file PWFBrowse.module.php
-# More info at http://dev.cmsmadesimple.org/projects/PWFBrowse
+# More info at http://dev.cmsmadesimple.org/projects/powerbrowse
 
 $padmin = $this->_CheckAccess('admin');
 $pmod = $this->_CheckAccess('modify');
