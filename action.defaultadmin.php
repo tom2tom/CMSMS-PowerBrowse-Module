6f3e247f33d6499832237f69e130c245b04e100d
tom2tom <tpgww@onepost.net>
2017-10-06 16:42:29 +1100

    revise settings-update to support async password-change

--- action.defaultadmin.php
+++ action.defaultadmin.php
@@ -13,28 +13,9 @@ if (!($padmin || $pmod || $pview)) {
 	exit;
 }
 
+$asyncpw = '';
 if (isset($params['submit'])) {
 	if ($padmin) {
-		$t = $params['rounds_factor'] + 0;
-		if ($t < 0.01) {
-			$t = 0.01;
-		} elseif ($t > 15) {
-			$t = 15;
-		}
-		$oldrounds = $this->GetPreference('rounds_factor') + 0;
-		$rehash = $oldrounds != $t;
-		$this->SetPreference('rounds_factor', $t);
-
-		$cfuncs = new PWFBrowse\Crypter($this);
-		$key = PWFBrowse\Crypter::MKEY;
-		$oldpw = $cfuncs->decrypt_preference($key);
-		$t = trim($params[$key]);
-		if ($oldpw != $t) {
-			$cfuncs->encrypt_preference($key, $t);
-			$cfuncs->encrypt_preference($key.'OLD', $oldpw);
-			$rehash = TRUE;
-		}
-
 		$this->SetPreference('date_format', trim($params['date_format']));
 		$this->SetPreference('export_file', !empty($params['export_file']));
 		$this->SetPreference('export_file_encoding', trim($params['export_file_encoding']));
@@ -60,13 +41,60 @@ if (isset($params['submit'])) {
 		}
 		$this->SetPreference('uploads_dir', $t);
 
+		$key = 'rounds_factor';
+		$i = $params[$key] + 0;
+		if ($i < 0.01) {
+			$i = 0.01;
+		} elseif ($i > 15) {
+			$i = 15;
+		}
+		$oldrounds = $this->GetPreference($key) + 0;
+		$rehash = $oldrounds != $i;
 		if ($rehash) {
-			//flag all records for update
+			$this->SetPreference($key, $i);
+		}
+
+		$asyncpw = $this->Lang('pending_password');
+
+		$cfuncs = new PWFBrowse\Crypter($this);
+		$key = PWFBrowse\Crypter::MKEY;
+		$oldpw = $cfuncs->decrypt_preference($key);
+		$newpw = trim($params[$key]);
+		if ($oldpw != $newpw && $newpw != $asyncpw) {
+			$rehash = TRUE;
+		} else {
+			$asyncpw = '';
+		}
+
+		if ($rehash) {
 			$pre = cms_db_prefix();
-			$sql = 'UPDATE '.$pre.'module_pwbr_record SET flags=1';
-			//initiate async update
-			$funcs = new PWFBrowse\RecordContent();
-			$funcs->StartUpdate();
+			$i = $db->GetOne('SELECT COUNT(1) FROM '.$pre.'module_pwbr_record');
+			if ($i > 0) {
+				if ($oldpw != $newpw) {
+					//new P/W id = unique, 1-byte, > 0 (i.e. not default)
+					$i = $db->GenID($pre.'module_pwbr_seq');
+					$i &= 0xff;
+					if ($i === 0) {
+						$i = 1;
+					}
+		 			$cfuncs->encrypt_preference('newpass'.$i, $newpw);
+					//remember, for later cleanup
+					$t = $this->GetPreference('newpasses');
+					if ($t) {
+						$t .= ','.$i;
+					} else {
+						$t = ''.$i;
+					}
+					$this->SetPreference('newpasses', $t);
+					$sql = 'UPDATE '.$pre.'module_pwbr_record SET newpass='.$i;
+					$db->Execute($sql);
+				}
+				//initiate async update
+				$funcs = new PWFBrowse\RecordContent();
+				$funcs->StartUpdate($this);
+			} elseif ($oldpw != $newpw) {
+	 			$cfuncs->encrypt_preference($key, $newpw);
+			}
 		}
 		$params['message'] = $this->_PrettyMessage('prefs_updated');
 	}
